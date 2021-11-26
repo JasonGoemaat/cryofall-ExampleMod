8a62aec37d258ffba1fd144469be868f22dc96a2
Jason Goemaat <jason.goemaat@gmail.com>
2021-11-26 16:07:03 -0600

    Split console commands into separate methods: hello, json

--- Scripts/ConsoleCommands/ExampleMod/ConsoleConsoleExampleMod.cs
+++ Scripts/ConsoleCommands/ExampleMod/ConsoleConsoleExampleMod.cs
@@ -29,10 +29,64 @@ namespace ExampleMod.ConsoleCommands.Console
             string searchCommand = null)
         {
             StringBuilder sb = new StringBuilder();
+            sb.AppendLine();
+
+            if (searchCommand != null && searchCommand.StartsWith("hello"))
+            {
+                DoHello(sb, searchCommand);
+                return sb.ToString();
+            }
+
+            if (searchCommand != null && searchCommand.StartsWith("json"))
+            {
+                DoJson(sb, searchCommand);
+                return sb.ToString();
+            }
+
+            sb.AppendLine();
+            sb.AppendLine("Usage:");
+            sb.AppendLine("    em hello - say hello");
+            sb.AppendLine("    em json - log json to chat log for your use");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// This is from CNEI - EntityViewModelsManager.cs
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private static string GetNameWithoutGenericArity(string s)
+        {
+            int index = s.IndexOf('`');
+            return index == -1 ? s : s.Substring(0, index);
+        }
+
+
+        private static IEnumerable<string> GetCommandNameSuggestions(string startsWith)
+        {
+            string[] commands = { "hello", "json" };
+            bool found = false;
+            foreach (string command in commands)
+            {
+                if (command.StartsWith(startsWith))
+                {
+                    yield return command;
+                }
+            }
+
+            yield return startsWith;
+        }
+
+        private static void DoHello(StringBuilder sb, string searchCommand)
+        {
             sb.AppendLine("Hello, world!");
             sb.AppendFormat("  searchCommand: '{0}'", searchCommand);
             sb.AppendLine();
+        }
 
+        private static void DoJson(StringBuilder sb, string searchCommand)
+        {
             // from looking at CNEI EntityViewModelsManager
             List<IProtoEntity> allEntitiesList = Api.FindProtoEntities<IProtoEntity>().ToList();
             // var entities = allEntitiesList.Where(x => x.Name.ToLowerInvariant().Contains("pepper")).ToList();
@@ -41,7 +95,7 @@ namespace ExampleMod.ConsoleCommands.Console
 
             List<ICustomSerializable> foods = new List<ICustomSerializable>();
 
-            foreach(var entity in entities)
+            foreach (var entity in entities)
             {
                 if (entity is ProtoItemFood)
                 {
@@ -61,26 +115,6 @@ namespace ExampleMod.ConsoleCommands.Console
 
             sb.AppendLine("}");
             sb.AppendLine("<---------- END SERIALIZATION ---------->");
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// This is from CNEI - EntityViewModelsManager.cs
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        private static string GetNameWithoutGenericArity(string s)
-        {
-            int index = s.IndexOf('`');
-            return index == -1 ? s : s.Substring(0, index);
-        }
-
-
-        private static IEnumerable<string> GetCommandNameSuggestions(string startsWith)
-        {
-            yield return "red";
-            yield return "green";
-            yield return "blue";
         }
     }
 }
